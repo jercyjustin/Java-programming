2cbcc81a786a68075c21f139e6bf9bd01aab676b
jercyjustin <43059409+jercyjustin@users.noreply.github.com>
2018-10-04 12:08:00 +0530

    Add files via upload

---
+++ positive or negative.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int number=3;
+    if(number > 0)
+    {
+    System.out.println("positive");  
+    }
+    else if(number < 0)
+    {
+    System.out.println("negative");
+  }
+    else if(number==0)
+    {
+    System.out.println(" zero");
+  }
+  else
+  {
+    System.out.println("positive or negative or zero");
+  }
+
+  }
+}
