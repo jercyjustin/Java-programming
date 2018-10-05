fd9f25b3d75e86b63857de745b0b387627d1acd5
jercyjustin <43059409+jercyjustin@users.noreply.github.com>
2018-10-05 12:33:51 +0530

    Add files via upload

---
+++ alphapet.java
@@ -0,0 +1,13 @@
+public class Alphabet{
+
+    public static void main(String[] args)
+     {
+
+        char c = 'd';
+
+        if (c >= 'a' && c <= 'z')
+            System.out.println("alphabet");
+        else
+            System.out.println("alphabet or not");
+    }
+}
