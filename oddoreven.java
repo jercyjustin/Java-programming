18286e68f961abf3a39469c38166c252ead37b00
jercyjustin <43059409+jercyjustin@users.noreply.github.com>
2018-10-05 12:00:05 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) 
+    {
+        int n=22212;
+        if(n % 2 == 0)
+        {
+            System.out.println(" Even ");
+        }
+        else
+        {
+            System.out.println(" Odd ");
+	}
+    }
+} 
+  
